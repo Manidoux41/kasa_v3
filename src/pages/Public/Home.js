67565996ae4450677e31ed511dad2fda93900358
Manidoux41 <lherissondu41@gmail.com>
2023-01-02 10:31:32 +0100

    add Card_container components

--- src/pages/Public/Home.js
+++ src/pages/Public/Home.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Banner from '../../components/Banner/Banner'
+import Cards from '../../components/Cards/Cards'
 
 const Home = () => {
   return (
     <div className='wrapper home'>
       <Banner />
+      <Cards>
+        <h2>Cards</h2>
       <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
       <p>test</p>
+      </Cards>
+      
     </div>
     
   )
