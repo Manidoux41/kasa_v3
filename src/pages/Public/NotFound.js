7d4b38cd1b3eb134ce8574b0d97c2deb4a9edea6
Manidoux41 <lherissondu41@gmail.com>
2023-01-03 12:27:54 +0100

    add Not found page

--- src/pages/Public/NotFound.js
+++ src/pages/Public/NotFound.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='notFound'>
       <h1>404</h1>
       <p>Oups! La page que vous demandez n'existe pas.</p>
       <NavLink to="/home">Retourner sur la page d'accueil</NavLink>
