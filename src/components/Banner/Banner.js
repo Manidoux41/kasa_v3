a1ba98b7f7558bcbb07d83aa5d20551dd01a23eb
Manidoux41 <lherissondu41@gmail.com>
2023-01-05 06:07:40 +0100

    add banner in About page

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,21 @@
 import React from 'react'
-import image1 from '../../assets/images/image1.jpg'
-
+import image1 from '../../assets/images/image1.jpg';
+import image2 from '../../assets/images/image2.jpg';
 const Banner = () => {
+
+  const image = [
+    {
+    src: image1,
+    width: '100%',
+    alt: 'image1'
+  },
+  {
+    src: image2,
+    width: '100%',
+    alt: 'image2'
+  }
+]
+
   return (
     <div className='banner'>
             <h1>Chez vous, partout et ailleurs</h1>
--- src/pages/Public/About.js
+++ src/pages/Public/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Banner from '../../components/Banner/Banner'
 import Dropdown from '../../components/Dropdown/Dropdown'
 
 const About = () => {
   return (
     <div className='wrapper about'>
+      <Banner />
       <Dropdown />
       <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
       <p>test</p>
