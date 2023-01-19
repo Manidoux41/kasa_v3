dd17fb3d67398cb223f243c422b14ff54b29356f
Manidoux41 <lherissondu41@gmail.com>
2023-01-19 22:15:26 +0100

    add slider

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import SlideLeft from "../../assets/svg/slide-left.svg";
 import SlideRight from "../../assets/svg/slide-right.svg";
 
-const SlideShow = (props) => {
+const SlideShow = ({ pictures }) => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+
+
+  const slideStyle = {
+    width:"100%",
+    height:"100%",
+    borderRadius: "10px",
+    backgroundPosition: "center",
+    backgroundSize:"cover",
+    backgroundImage: `url(${pictures[currentIndex]})`,
+    position:"relative" 
+  }
+
+  const goToPrevious = () => {
+    const isFirstPicture = currentIndex === 0
+    const newIndex = isFirstPicture ? pictures.length -1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  }
+
+  const goToNext = () => {
+    const isLastPicture = currentIndex === pictures.length -1
+    const newIndex = isLastPicture ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  }
+
   return (
-    <div className="slide__show">
-      <div className="slide__gallery">
-        <img src={props.pictures[0]} alt="" />
-      </div>
+    <div style={slideStyle}    >
       <div className="slide__left">
-        <img src={SlideLeft} alt="slide left" />
+        <img src={SlideLeft} alt="slide left" onClick={goToPrevious}/>
       </div>
       <div className="slide__right">
-        <img src={SlideRight} alt="slide right" />
+        <img src={SlideRight} alt="slide right" onClick={goToNext}/>
       </div>
     </div>
   );
--- src/pages/Public/Logements.js
+++ src/pages/Public/Logements.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import SlideShow from "../../components/SlideShow/SlideShow";
 import data from "../../datas/appartement.json";
@@ -6,16 +6,25 @@ import data from "../../datas/appartement.json";
 const Logements = () => {
   const { logementId } = useParams();
   const appart = data.find((appart) => appart.id === logementId);
-  const { title, location, pictures,tags,host } = appart;
+  const { title, location, pictures, tags, host, rating } = appart;
+
+  const containerStyles = {
+    with:"100%",
+    height: "415px",
+    margin:"0 auto"
+  }
+
 
   return (
     <div className="wrapper logements">
-      <SlideShow pictures={pictures}/>
+      <div style={containerStyles}>
+        <SlideShow pictures={pictures} />
+      </div>
       <div className="logementHeader">
         <div className="logementInfo">
           <h1>{title}</h1>
           <p>{location}</p>
-          <ul className="tag">            
+          <ul className="tag">
             {tags.map((tag, index) => (
               <li key={index}>{tag}</li>
             ))}
@@ -28,14 +37,10 @@ const Logements = () => {
               <img src={host.picture} alt="" />
             </div>
           </div>
-          <div className="stars">
-            
-          </div>
+          <div className="stars">{rating}</div>
+        </div>
       </div>
-      </div>
-      <div className="description">
-
-      </div>      
+      <div className="description"></div>
     </div>
   );
 };
