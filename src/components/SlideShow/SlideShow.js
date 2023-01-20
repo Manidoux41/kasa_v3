cd2c4f7f9e30f72ccccc98bad5128aa15445f084
Manidoux41 <lherissondu41@gmail.com>
2023-01-20 13:52:58 +0100

    update responsive page

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -5,41 +5,44 @@ import SlideRight from "../../assets/svg/slide-right.svg";
 const SlideShow = ({ pictures }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-
   const slideStyle = {
-    width:"100%",
-    height:"100%",
+    width: "100%",
+    height: "100%",
     borderRadius: "10px",
     backgroundPosition: "center",
-    backgroundSize:"cover",
+    backgroundSize: "cover",
     backgroundImage: `url(${pictures[currentIndex]})`,
-    position:"relative" 
-  }
+    position: "relative",
+  };
 
   const goToPrevious = () => {
-    const isFirstPicture = currentIndex === 0
-    const newIndex = isFirstPicture ? pictures.length -1 : currentIndex - 1;
+    const isFirstPicture = currentIndex === 0;
+    const newIndex = isFirstPicture ? pictures.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
-  }
+  };
 
   const goToNext = () => {
-    const isLastPicture = currentIndex === pictures.length -1
+    const isLastPicture = currentIndex === pictures.length - 1;
     const newIndex = isLastPicture ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
-  }
+  };
 
   return (
-    <div style={slideStyle}>
-      <div className="slide__left">
-        <img src={SlideLeft} alt="slide left" onClick={goToPrevious}/>
+    
+      <div style={slideStyle}>
+        <div className="slide__left">
+          <img src={SlideLeft} alt="slide left" onClick={goToPrevious} />
+        </div>
+        <div className="slide__right">
+          <img src={SlideRight} alt="slide right" onClick={goToNext} />
+        </div>
+        <div className="position">
+          <p>
+            {currentIndex + 1}/{pictures.length}
+          </p>
+        </div>
       </div>
-      <div className="slide__right">
-        <img src={SlideRight} alt="slide right" onClick={goToNext}/>
-      </div>
-      <div className="position">
-        <p>{currentIndex + 1}/{pictures.length}</p>
-      </div>
-    </div>
+    
   );
 };
 
