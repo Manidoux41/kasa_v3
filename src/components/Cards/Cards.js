ba0a3ee28cc08b0d95f19a3fdec53c82123b46c3
Manidoux41 <lherissondu41@gmail.com>
2023-01-27 13:27:26 +0100

    add useEffect

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,33 +1,21 @@
 import React from 'react'
 import Card from './Card'
 import data from '../../datas/appartement.json';
+import { useState, useEffect } from 'react';
 
 
 
 const Cards = () => {  
 
-// const  [data, setData] = useState([])
+const [apparts, setApparts] = useState([])
 
-// useEffect(() => {
-//   const url = "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json"
-
-//   const fetchData = async () => {
-//     try {
-//       const response = await fetch(url)
-//       const json = await response.json()
-//       console.log(json);
-//     } catch (error) {
-//       console.log("error", error);
-//     }
-//   }
-
-//   fetchData()
-
-// }, [])
+useEffect(() => {
+  setApparts(data)
+}, [apparts])
 
   return (
     <div className='cards'>
-        {data.map((appart) => (
+        {apparts.map((appart) => (
         <Card key={appart.id} appart={appart} />
       ))}
     </div>
