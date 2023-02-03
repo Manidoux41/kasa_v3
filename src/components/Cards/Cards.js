fd392f786a3a7966a43cca05ed6cba66e4d43d31
Manidoux41 <lherissondu41@gmail.com>
2023-02-03 22:13:55 +0100

    use fetch for retrieve data in card component

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,25 +1,48 @@
 import React from 'react'
 import Card from './Card'
-import data from '../../datas/appartement.json';
+//import data from '../../datas/appartement.json';
 import { useState, useEffect } from 'react';
 
 
 
+// const Cards = () => {  
+
+// const [apparts, setApparts] = useState([])
+
+// useEffect(() => {
+//   setApparts(data)
+// }, [apparts])
+
+//   return (
+//     <div className='cards'>
+//         {apparts.map((appart) => (
+//         <Card key={appart.id} appart={appart} />
+//       ))}
+//     </div>
+//   )
+// }
+
 const Cards = () => {  
 
-const [apparts, setApparts] = useState([])
-
-useEffect(() => {
-  setApparts(data)
-}, [apparts])
-
-  return (
-    <div className='cards'>
-        {apparts.map((appart) => (
-        <Card key={appart.id} appart={appart} />
-      ))}
-    </div>
-  )
-}
+  const [apparts, setApparts] = useState([])
+  
+  useEffect(() => {
+    try {
+      fetch('/data/appartement.json')
+      .then(response => response.json())
+      .then(jsonData => setApparts(jsonData))
+    } catch(error) {
+      console.error(error)
+    }
+  }, [apparts])
+  
+    return (
+      <div className='cards'>
+          {apparts.map((appart) => (
+          <Card key={appart.id} appart={appart} />
+        ))}
+      </div>
+    )
+  }
 
 export default Cards
