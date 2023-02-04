b109d19865777a369823b3baa9a3f0b79a846105
Manidoux41 <lherissondu41@gmail.com>
2023-02-04 05:45:39 +0100

    set asynchronous message handler

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -25,30 +25,34 @@ import { useState, useEffect } from 'react';
 const Cards = () => {  
 
   const [apparts, setApparts] = useState([])
-  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   
   useEffect(() => {   
-    const intervalId = setInterval(async () =>{
-      await fetch('/data/appartement.json')
-      .then(response => response.json())
-      .then(jsonData => {
-        setApparts(jsonData)
-        setIsLoading(true)
-        clearInterval(intervalId)
-      })
-      .catch(error => {
-      console.error(error)
-      setIsLoading(false);
-      clearInterval(intervalId)
-    })
-    }, 2000) 
-    return () => {
-      clearInterval(intervalId)
-    }
-  }, [apparts])
+    const fetchData = () => {
+      fetch('/data/appartement.json')
+        .then(response => response.json())
+        .then(jsonData => {
+          setApparts(jsonData);
+          setIsLoading(false);
+        })
+        .catch(error => {
+          setError(error);
+          setIsLoading(false);
+        });
+    };
+
+    setTimeout(fetchData, 0);
+
+    return () => {};
+  }, []);
+
+  if (error) {
+    return <p>An error occurred: {error.message}</p>;
+  }
   
-    return isLoading ? (
+    return !isLoading ? (
       <div className='cards'>
           {apparts.map((appart) => (
           <Card key={appart.id} appart={appart} />
