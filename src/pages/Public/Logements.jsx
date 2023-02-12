68a7bbf41f18335a3fd4fd7f817ff40590408aa2
Manidoux41 <lherissondu41@gmail.com>
2023-02-12 18:03:55 +0100

    modif logement page

--- src/pages/Public/Logements.jsx
+++ src/pages/Public/Logements.jsx
@@ -5,7 +5,7 @@ import SlideShow from "../../components/SlideShow/SlideShow";
 import data from "../../datas/appartement.json";
 import NotFound from "./NotFound";
 
-const Logements = ({ children }) => {
+const Logements = () => {
 
 const { logementId } = useParams();
 const appart = data.find((appart) => appart.id === logementId);  
